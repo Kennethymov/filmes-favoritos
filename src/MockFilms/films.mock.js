fec4ad7f6bd6af60be2bc286b4b3a5ed3ba0650f
Kennethy <kennethymov@gmail.com>
2023-01-25 10:52:11 -0300

    initial config

---
+++ src/MockFilms/films.mock.js
@@ -0,0 +1,57 @@
+export const films = [
+  {
+    name: "Harry Potter e a Pedra Filosofal",
+    diretor: "Chris Columbus",
+    country: "Inglaterra",
+    release_year: 2001,
+    image: "/images/hp1.jpeg"
+  },
+  {
+    name: "Harry Potter e a Câmara Secreta",
+    diretor: "Chris Columbus",
+    country: "Inglaterra",
+    release_year: 2002,
+    image: "/images/hp2.jpeg"
+  },
+  {
+    name: "Harry Potter e o Prisioneiro de Azkaban",
+    diretor: "Alfonso Cuarón",
+    country: "Inglaterra",
+    release_year: 2004,
+    image: "/images/hp3.jpeg"
+  },
+  {
+    name: "Harry Potter e o Cálice de Fogo",
+    diretor: "Mike Newell",
+    country: "Inglaterra",
+    release_year: 2005,
+    image: "/images/hp4.jpeg"
+  },{
+    name: "Harry Potter e a Ordem da Fênix",
+    diretor: "David Yates",
+    country: "Inglaterra",
+    release_year: 2007,
+    image: "/images/hp5.jpeg"
+  },
+  {
+    name: "Harry Potter e o Enigma do Príncipe",
+    diretor: "David Yates",
+    country: "Inglaterra",
+    release_year: 2009,
+    image: "/images/hp6.jpeg"
+  },
+  {
+    name: "Harry Potter e as Relíquias da Morte: Parte 1",
+    diretor: "David Yates",
+    country: "Inglaterra",
+    release_year: 2010,
+    image: "/images/hp7-1.jpeg"
+  },
+  {
+    name: "Harry Potter e as Relíquias da Morte: Parte 2",
+    diretor: "David Yates",
+    country: "Inglaterra",
+    release_year: 2011,
+    image: "/images/hp7-2.jpeg"
+  }
+]
